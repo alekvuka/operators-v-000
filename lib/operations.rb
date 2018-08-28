f335fcf9c638875e4bc0cb0dd0aaa5b49d8ccca1
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-28 02:51:26 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,6 @@
+require 'pry'
+
+
 def unsafe?(speed)
 
 	if speed < 40 || speed > 60
@@ -13,6 +16,6 @@ end
 def not_safe?(speed)
 
 speed < 40 ? true : false
-speed > 60 ? true : false 
+speed > 60 ? true : false
 
 end
